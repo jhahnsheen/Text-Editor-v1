9e1cc07ddf4722e6290c7234de1284b0e29b41a7
jhahnsheen <91440103+jhahnsheen@users.noreply.github.com>
2022-02-22 22:34:56 -0500

    pushing to heroku

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Text-Editor-v1
+# J.A.T.E. Text Editor v1
+
+The J.A.T.E. Text Editor is a minimalist-style text editor progressive web app (PWA) that can be downloaded and used on the desktop, even when offline.
+
+## Installation
+
+the J.A.T.E. Text Editor download page is hosted on Heroku at [this link](filler text). Simply go to the page and click the install button to access the J.A.T.E. Text Editor remotely on your personal device.
+
+## Usage
+
+Usage of the J.A.T.E. Text Editor is very simple. Upon initiating the web app, you should see a screen like this:
+
+All you need to do is type in the designated area and all of the text that you type will be available to review and for editing. 
+
+## Contributions
+
+The GitHub repo containing the code can be found [here](https://github.com/jhahnsheen/Text-Editor-v1). Please feel free to take a look at the code. 
+
+This code is only to be used for development and educational purposes. 
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,56 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // Add HtmlWebpackPlugin
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'J.A.T.E. Text Editor'
+      }),
+      // Add WebpackPwaManifest
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'J.A.T.E. Text Editor',
+        short_name: 'J.A.T.E.',
+        description: 'Simple text editor',
+        background_color: '#F1F3F5',
+        theme_color: '#243758',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            scr: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+      // Add InjectManifest
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw,js',
+      }),
     ],
 
     module: {
       rules: [
-        
+        //Adding CSS Loader Module
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader']
+        },
+        // Adding Babel Module
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
